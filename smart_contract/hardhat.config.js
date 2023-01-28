c0eecee2ac4760b39ee08fbbb130902b3ded0042
Yog Raj Paudel <yograjpaudel4241@gmail.com>
2023-01-28 22:22:07 +0545

    env

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,8 +1,7 @@
-// https://eth-mainnet.g.alchemy.com/v2/OHzR8b83FBOpQ8jnE9CcO1WinNizRiQe
 require("@nomiclabs/hardhat-waffle");
 
-const ALCHEMY_API_KEY = "OHzR8b83FBOpQ8jnE9CcO1WinNizRiQe";
-const GOERLI_PRIVATE_KEY = "da13860b4c62cf9d447db319aac5d2d859a2ac7f75fd4080a347c7aa31c02133";
+const ALCHEMY_API_KEY = import.meta.env.VITE_ALCHEMY_API_KEY;
+const GOERLI_PRIVATE_KEY = import.meta.env.VITE_GOERLI_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.9",
